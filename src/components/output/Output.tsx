435b5b7e45271c90cce914954e64e9d749143871
kriss <krzysztof.podmokly@gmail.com>
2024-03-01 16:18:19 +0100

    Update types

--- src/components/output/Output.tsx
+++ src/components/output/Output.tsx
@@ -1,6 +1,14 @@
+import { ICountry } from "../input/interface";
 import styles from "./output.module.scss";
 
-const Output = ({ data }: any) => {
+const Output = ({
+  data,
+}: {
+  data: {
+    activationCountries: ICountry[];
+    restrictedCountries: ICountry[];
+  };
+}) => {
   return (
     <div className={styles.wrapper}>
       <h3>Form payload</h3>
